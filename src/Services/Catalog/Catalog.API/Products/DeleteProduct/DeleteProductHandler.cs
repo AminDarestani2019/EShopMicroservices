16d07a97d343e8ecde827cda6d38b05c97c33420
Amin Darestani <darestani.amin@gmail.com>
2025-04-28 08:06:16 +0330

    adding delete and update validation

--- src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -4,6 +4,14 @@ namespace Catalog.API.Products.DeleteProduct;
 public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
 public record DeleteProductResult(bool IsSuccess);
 
+public class DeleteProductCammandValidator : AbstractValidator<DeleteProductCommand>
+{ 
+    public DeleteProductCammandValidator() 
+    {
+        RuleFor(x => x.Id).NotEmpty().WithMessage("Product ID is required");
+    }
+}
+
 internal class DeleteProductCommandHandler
     (IDocumentSession session, ILogger<DeleteProductCommandHandler> logger)
     : ICommandHandler<DeleteProductCommand, DeleteProductResult>
