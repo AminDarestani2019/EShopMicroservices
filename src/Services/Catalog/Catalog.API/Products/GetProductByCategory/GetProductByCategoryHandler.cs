31f48e0a8110a7c5b152a2782c560d5d625252fe
Amin Darestani <darestani.amin@gmail.com>
2025-04-28 14:02:49 +0330

    adding health check

--- src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -4,12 +4,11 @@ public record GetProductByCategoryQuery(string Category):IQuery<GetProductByCate
 public record GetProductByCategoryResult(IEnumerable<Product> Products);
 
 internal class GetProductByCategoryQueryHandler
-    (IDocumentSession session, ILogger<GetProductByCategoryQueryHandler> logger)
+    (IDocumentSession session)
     : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
 {
     public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
     {
-        logger.LogInformation("GetProductByCategoryQueryHandler.Handle called with {@query}",query);
         var products = await session.Query<Product>().Where(p=>p.Category.Contains(query.Category)).ToListAsync();
         return new GetProductByCategoryResult(products);
     }
